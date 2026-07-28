8da798fc76128e9e2666face95acd57865248903
zohair636 <hafiz.zohair2@gmail.com>
2026-07-28 23:32:48 +0500

    refactor(landing-page): remove common-breadcrumb component usage

--- src/pages/public/landing.tsx
+++ src/pages/public/landing.tsx
@@ -1,37 +1,7 @@
-import { CommonBreadCrumb } from "@/components/shared/breadcrumb";
-
 const Landing = () => {
-  const items = [
-    {
-      id: 1,
-      label: "Home",
-      href: "/",
-    },
-    {
-      id: 2,
-      label: "About",
-      href: "/about",
-    },
-    {
-      id: 3,
-      label: "Contact",
-      href: "/about",
-    },
-    {
-      id: 4,
-      label: "Profile",
-      href: "/profile",
-    },
-    {
-      id: 5,
-      label: "Account",
-      href: "/account",
-    },
-  ];
   return (
     <div>
       <p>Landing Page</p>
-      <CommonBreadCrumb items={items} />
     </div>
   );
 };
