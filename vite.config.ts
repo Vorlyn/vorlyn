0ab4783e3a17ec1f03c97119c379979252e7812b
zohair636 <hafiz.zohair2@gmail.com>
2026-07-31 23:59:45 +0500

    chore: setup Vite 8 compatability

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,36 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
   build: {
-    rollupOptions: {
+    minify: "oxc",
+    sourcemap: false,
+    rolldownOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react-router-dom', '@tanstack/react-query']
-        }
-      }
-    }
-  }
-})
+        codeSplitting: {
+          groups: [
+            {
+              name: "vendor",
+              test: /[\\/]node_modules[\\/](react-router-dom|@tanstack\/react-query|@tanstack\/react-table|date-fns)[\\/]/,
+              priority: 10,
+            },
+            {
+              name: "ui-frameworks",
+              test: /[\\/]node_modules[\\/]@base-ui\/react[\\/]/,
+              priority: 10,
+            },
+          ],
+        },
+      },
+    },
+  },
+});
