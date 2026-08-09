ea4b03e0d244e2904f1a07c653cd46406ebe0cdc
zohair636 <hafiz.zohair2@gmail.com>
2026-08-09 14:53:31 +0500

    feat(api): add response interceptor for global error handling

--- src/config/axios-instance.ts
+++ src/config/axios-instance.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { env } from "./env";
-import { getLocalStorage } from "@/utils/storage-utils";
+import { getLocalStorage, removeLocalStorageItem } from "@/utils/storage-utils";
+import { showErrorSonner } from "@/components/shared/sonner";
 
 const axiosInstance = axios.create({
   baseURL: env.VITE_API_BASE_URL,
+  timeout: 15000,
 });
 axiosInstance.interceptors.request.use(
   (config) => {
@@ -13,6 +15,23 @@ axiosInstance.interceptors.request.use(
     }
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      removeLocalStorageItem(env.VITE_AUTH_TOKEN_SECRET);
+    }
+    if (!error.response) {
+      showErrorSonner({
+        message: "Network Error",
+        description: "Please check your internet connection and try again.",
+      });
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default axiosInstance;
