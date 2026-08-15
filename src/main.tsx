d0966ed0420f8fb72d865e33d032e09502c36dfe
zohair636 <hafiz.zohair2@gmail.com>
2026-08-15 23:33:28 +0500

    feat(main): sync logout accross tabs via storage event

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "./components/ui/tooltip.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
+import { env } from "./config/env.ts";
+import { showInfoSonner } from "./components/shared/sonner/index.ts";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,6 +23,16 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
     },
   },
+});
+
+window.addEventListener("storage", (event) => {
+  if (event.key === env.VITE_AUTH_TOKEN_SECRET && event.newValue === null) {
+    showInfoSonner({
+      message: "Your session has been expired!",
+      description: "Please login again to continue your actions.",
+    });
+    window.location.href = "/auth/login";
+  }
 });
 
 createRoot(document.getElementById("root")!).render(
