c9081cb847692a51858bbbd1d8c16f847e8a9703
zohair636 <hafiz.zohair2@gmail.com>
2026-08-24 00:43:57 +0500

    chore: migrate utils package to packages/utils

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,8 @@
+export { cn } from "./cn";
+export { preventTriggerPointerDown } from "./event-handlers";
+export {
+  setLocalStorage,
+  getLocalStorage,
+  removeLocalStorageItem,
+  removeAllFromLocalStorage,
+} from "./storage-utils";
